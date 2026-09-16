ea06e7c8334971045347e39e9108b6329bd5de71
Hung-497 <giahung092007@gmail.com>
2026-09-16 11:30:30 +0300

    feat(inquiry): complete PBI-05 seller contact backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const propertyRouter = require('./routes/propertyRouter');
-const favouritesRouter = require('./routes/favouritesRouter')
-const moderationRouter = require('./routes/moderationRouter');
-const userRouter = require('./routes/userRouter');
-const loginRouter = require('./routes/loginRouter');
-const verificationRouter = require('./routes/verificationRouter');
+const connectDB = require("./config/db");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
+const propertyRouter = require("./routes/propertyRouter");
+const favouritesRouter = require("./routes/favouritesRouter");
+const moderationRouter = require("./routes/moderationRouter");
+const userRouter = require("./routes/userRouter");
+const loginRouter = require("./routes/loginRouter");
+const verificationRouter = require("./routes/verificationRouter");
+const inquiryRouter = require("./routes/inquiryRouter");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -16,12 +18,19 @@ app.use(express.json());
 connectDB();
 
 // Use the propertyRouter for all /properties routes
-app.use('/api/properties', propertyRouter);
-app.use('/api/favourites', favouritesRouter);
-app.use('/api/moderation', moderationRouter);
-app.use('/api/users', userRouter);
-app.use('/api/account', loginRouter);
-app.use('/api/verifications', verificationRouter);
+app.use("/api/properties", propertyRouter);
+app.use("/api/favourites", favouritesRouter);
+app.use("/api/moderation", moderationRouter);
+app.use("/api/users", userRouter);
+app.use("/api/account", loginRouter);
+app.use("/api/verifications", verificationRouter);
+app.use("/api/inquiries", inquiryRouter);
+
+// Middleware for handling unknown endpoints
+app.use(unknownEndpoint);
+
+// Middleware for handling errors
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 // Start the server
