7dbe825e6c02b5cb220bfd2e448834c6257d5d73
Hung-497 <giahung092007@gmail.com>
2026-09-16 21:11:01 +0300

    refactor: move Sprint 2 frontend into frontend directory

--- KotiSpot/src/pages/ContactThankMessage.jsx
+++
@@ -1,7 +0,0 @@
-const ContactThankMessage = () => {
-    return(
-        <div className = "thank-you-message">Thank you for your message! We will get back to you as soon as possible.</div>
-    )
-}
-
-export default ContactThankMessage;
